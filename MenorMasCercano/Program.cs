186d5fc180943c804b2a2ef91c9916650a80269c
Nicolas Mendez <nicomenfede@gmail.com>
2020-04-17 13:16:32 +0100

    Print Previous Smaller

---
+++ MenorMasCercano/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MenorMasCercano
+{
+    class Program
+    {
+        public static string NearestSmallerValues(int[] arr)
+        {
+
+            // code goes here
+
+            var result = "-1 ";
+            for (int i = 1; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+                for (int j = i - 1; j == 0; j--)
+                {
+                    Console.WriteLine("j = " + j + " i = " + i);
+                    if (arr[j] < arr[i])
+                    {
+                        result = result + arr[j] + " ";
+                        break;
+                    }
+                    result = result + "-1 ";
+                    Console.WriteLine("Pase");
+                }
+            }
+
+            return result;
+
+        }
+        static void Main(string[] args)
+        {
+            var entrada = new int[] { 5, 3, 1, 5, 5, 8 };
+            Console.WriteLine(NearestSmallerValues(entrada));
+        }
+    }
+}
